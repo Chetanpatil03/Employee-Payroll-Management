daec76706ab0990033f51b48e5522a83027e6bab
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-10 02:15:58 +0530

    Created FullTimeEmployee and PartTimeEmployee classes for employees and extend both of them from Employee class

--- src/Main.java
+++ src/Main.java
@@ -16,9 +16,44 @@ abstract class Employee{
         return id;
     }
 
-    abstract
+    abstract double calculateSalary();
+
+    @Override
+    public String toString() {
+        String s = "Employee{" + "name='" + name + '\'' + ", id=" + id + ",salary = \"calculateSalary()+\"}";
+        return s;
+    }
+}
+class FullTimeEmployee extends Employee  {
+    private double MonthlySalary;
+
+    public FullTimeEmployee(String name, int id, double MonthlySalary){
+        super(name,id);
+        this.MonthlySalary = MonthlySalary;
+    }
+
+    @Override
+    public double calculateSalary(){
+        return  MonthlySalary;
+    }
+
 }
 
+class  PartTimeEmployee extends Employee{
+    private int hoursWork;
+    private double hourlyRate;
+
+    public PartTimeEmployee(String name, int id, double hourlyRate,int hoursWork){
+        super(name,id);
+        this.hourlyRate = hourlyRate;
+        this.hoursWork = hoursWork;
+    }
+
+    @Override
+    public double calculateSalary(){
+        return  hoursWork * hourlyRate;
+    }
+}
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
